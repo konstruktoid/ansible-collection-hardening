fc6fcb55a8031c128d30d581d97cf96e4ff1e1d1
Thomas Sjögren <konstruktoid@users.noreply.github.com>
2026-06-18 19:44:14 +0000

    more tests
    Signed-off-by: Thomas Sjögren <konstruktoid@users.noreply.github.com>

--- scripts/add_tests.sh
+++ scripts/add_tests.sh
@@ -2,6 +2,6 @@
 
 grep -vE '^$|#' extensions/molecule/resources/converge.yml | grep '\- role:' | awk -F '.' '{print $NF}' | while read -r r; do
   if ! grep -q "name: Verify ${r} role" extensions/molecule/resources/verify.yml; then
-    echo -e "\n    - name: Verify ${r} role\n      ansible.builtin.import_tasks:\n        file: ../tests/verify_${r}.yml"
+    echo -e "\n    - name: Verify ${r} role\n      ansible.builtin.include_tasks:\n        file: ../tests/verify_${r}.yml"
   fi
 done
